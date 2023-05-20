521090c853d2b47a03955ff99229664ba914101c
emga9xkc2 <android.24081995@gmail.com>
2023-05-20 14:30:01 +0700

    commit

--- web/static/myscript.js
+++ web/static/myscript.js
@@ -301,7 +301,32 @@ function sleep(ms) {
 includeHTML();
 // CheckLogin();
 editClassActive();
+function sortTable(idTable, columnIndex) {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById(idTable);
+  switching = true;
 
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+
+    for (i = 1; i < rows.length - 1; i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[columnIndex];
+      y = rows[i + 1].getElementsByTagName("TD")[columnIndex];
+
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
 function showTable(options = {}) {
   return new Promise(function (resolve, reject) {
     var overlay = document.createElement("div");
